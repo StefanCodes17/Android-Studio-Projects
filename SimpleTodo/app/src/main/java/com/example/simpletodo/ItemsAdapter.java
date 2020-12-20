b8de7eaafc3f2a0563b10c6985795f3bd9b90969
Stefan Kolev <sk893q@gmail.com>
2020-12-19 21:04:18 -0500

    Finished basic add/remove logic for android app

--- SimpleTodo/app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ SimpleTodo/app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -3,6 +3,7 @@ package com.example.simpletodo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,10 +13,16 @@ import java.util.List;
 // Responsible for displaying data from the model into a row in the recycler view
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
 
-    List<String> items;
+    public interface OnLongClickListener{
+        void onItemLongClicked(int position);
+    }
 
-    public ItemsAdapter(List<String> items){
+    List<String> items;
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener){
         this.items = items;
+        this.longClickListener = longClickListener;
     }
 
     @NonNull
@@ -23,27 +30,46 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
     // Creates each view
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Use layout inflater to inflate a view
-        LayoutInflater.from()
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         // wrap it inside a View Holder and return it
-        return null;
+        return new ViewHolder(todoView);
     }
 
     @Override
     // Binds data into view holder
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        //Grab the item at position
+        String item = items.get(position);
+        //Bind the item into the specified view holder
+        holder.bind(item);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     // Container to provide access to views that represent each row of the list
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView tvItem;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+        //Update view inside the view holder with this data
+        public void bind(String item) {
+
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //Notify the listener which position was long pressed
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
         }
     }
 }
--- SimpleTodo/app/src/main/java/com/example/simpletodo/MainActivity.java
+++ SimpleTodo/app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd;
     EditText etItem;
     RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +36,57 @@ public class MainActivity extends AppCompatActivity {
         etItem = findViewById(R.id.todo__item__text__field);
         rvItems = findViewById(R.id.rv__items);
 
-        etItem.setText("Wow this is Java!");
+        loadItems();
 
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Go to the gym");
-        items.add("Code the todo app");
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
+            @Override
+            public void onItemLongClicked(int position) {
+                //Delete the item from the model
+                items.remove(position);
+                //Notify the adapter at which position the item was deleted
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener );
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = etItem.getText().toString();
+                //Ad item to the model
+                items.add(todoItem);
+                //Notify adapter that an item has been inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item added successfully", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+    }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+    //This function will load items from txt file
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items!", e);
+            items = new ArrayList<>();
+        }
+    }
+    //This function writes the file
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items!", e);
+        }
     }
 }
